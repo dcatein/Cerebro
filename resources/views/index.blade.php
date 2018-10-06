2d3e658c19ecce9e170fd3e93f9a493ca16dde27
yssacst <rayssacoosta@gmail.com>
2018-10-06 08:37:22 -0300

    Estrutura de Layout

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+
+@extends('layout.mainlayout')
+
+@section('content')
+
+  <div class="album text-muted">
+
+    <div class="container">
+
+      <div class="row">
+
+        oi
+      </div>
+
+    </div>
+
+  </div>
+
+@endsection
+
+
+
+
+
+
+<b></b>
---
+++ resources/views/layout/partials/head.blade.php
@@ -0,0 +1,26 @@
+<meta charset="utf-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<meta name="description" content="">
+
+<meta name="author" content="">
+
+
+
+<title>Album example for Bootstrap</title>
+
+
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+
+<meta name="description" content="">
+
+<meta name="author" content="">
+
+<title>CEREBRO</title>
