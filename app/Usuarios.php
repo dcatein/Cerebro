9e20b60d33b60586b1b383244488c409abdf84e3
Davidson Catein <dcatein1@gmail.com>
2018-10-11 17:28:09 -0300

    CRUD Usuarios

---
+++ app/Usuarios.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace cerebro;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Usuarios extends Model
+{
+    protected $table = 'usuarios';
+    public $timestamps = false;
+    
+    protected $fillable = array('nome','idade','email','senha');
+    protected $guarded = ['id'];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+// Rotas GET
 
 Route::get('/', function(){
     return 'Rota padrão laravel';
@@ -10,5 +8,13 @@ Route::get('/', function(){
 
 
 Route::get('/usuarios', 'UsuariosController@index');
-
 Route::get('/usuarios/show/{id}', 'UsuariosController@find');
+Route::get('/usuarios/novo', 'UsuariosController@novo');
+Route::get('/usuarios/delete/{id}', 'UsuariosController@delete');
+Route::get('/usuarios/alterar/{id}', 'UsuariosController@alterar');
+//-----
+
+//Rotas POST
+Route::post('/usuarios/insert', 'UsuariosController@insert');
+Route::post('/usuarios/update/{id}', 'UsuariosController@update');
+//----- 
