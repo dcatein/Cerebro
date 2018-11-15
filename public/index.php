6aeb118b84f503fa5c91ca183a9f1fd0a9ce5743
Davidson Catein <dcatein1@gmail.com>
2018-11-15 12:34:24 -0200

    Heroku 21

--- public/index.php
+++ public/index.php
@@ -58,32 +58,3 @@ $response = $kernel->handle(
 $response->send();
 
 $kernel->terminate($request, $response);
-
-$dbopts = parse_url(getenv('DATABASE_URL'));
-$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-               array(
-                'pdo.server' => array(
-                   'driver'   => 'pgsql',
-                   'user' => $dbopts["user"],
-                   'password' => $dbopts["pass"],
-                   'host' => $dbopts["host"],
-                   'port' => $dbopts["port"],
-                   'dbname' => ltrim($dbopts["path"],'/')
-                   )
-               )
-);
-
-$app->get('/db/', function() use($app) {
-    $st = $app['pdo']->prepare('SELECT * FROM grupos');
-    $st->execute();
-  
-    $nomes = array();
-    while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-      $app['monolog']->addDebug('Row ' . $row['nome']);
-      $nomes[] = $row;
-    }
-  
-    return $app['twig']->render('database.twig', array(
-      'nomes' => $nomes
-    ));
-  });
