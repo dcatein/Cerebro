40ff1bf144025c09fee6d1ebd05cf2f9edf827e3
yssacst <38430578+yssacst@users.noreply.github.com>
2018-11-15 10:36:11 -0200

    Rayeditardeletarcomentariopostagem (#18)
    * taFazendoPostagensEComentarios
    
    * botaoeditardeletarcomentariopostagem

--- resources/views/grupos/show.blade.php
+++ resources/views/grupos/show.blade.php
@@ -2,7 +2,7 @@
     @section('content')
 
 
-<div class="container-fluid pt-2 px-5">
+<div class="container-fluid pt-2 px-4">
     <div class="row justify-content-end ">
         <div class="col-md-12 pl-5" style="display:inline-block">   
             <div class="row"><!--cabeçalho-->
@@ -21,27 +21,28 @@
                         </div>
                     </div><!--subtitulo-->
                 </div>
-                <div class="col-md-4 pt-2" style="display:inline-block">
+                <div class="col-md-4 col-sm pt-2" style="display:inline-block">
                     <div class="row">
-                        <div class="col-md center">
+                        <div class="col center">
                             <div title="Componentes" class="fa fa-group">
                                 <p class="icon-notify">4</p>
                             </div>
                         </div>
                         
-                        <div class="col-md center">
+                        <div class="col center">
                             <div title="Comentários" class="fa fa-commenting ">
                                 <p class="icon-notify">4</p>
                             </div>
                         </div>
+
                         <div class="col-md center"">
                             <a href="{{action('GruposController@convidarUsuario',$grupo['grupo']->id)}}" title="Convide um amigo" class="fa fa-user-plus ">
                                 <p class="icon-notify " >Convide</p>
                             </a>
                         </div>
-                        <div class="col-md center" >
+                        <div class="col center" >
                             <a href="{{action('GruposController@alterar',$grupo['grupo']->id)}}" title="Configurações" class="fa fa-cogs">
-                                <p class="icon-notify">Configurações</p>
+                                <p id="lbl-config" class="icon-notify">Configurações</p>
                             </a>
                         </div>
                     </div>
@@ -49,6 +50,7 @@
             </div><!--cabeçalho-->
             
             <div class="row"><!--corpo-->
+
                 <div class="col-md-4">
                     <div class="container-group my-2 py-2 px-2 center""><!-- escrever comentarios-->  
                         <div class="row" ; >
@@ -72,6 +74,7 @@
 
                             <div class="form-group ">
                                 <input type="text" name="palavras" placeholder="Palavras-chave separadas por ponto e vírgula (;)" class="form-control form-control-sm" />
+
                             </div>
 
                                 <button type="submit" class="btn btn-default" style="background-color: #2e8f63; color: #ffffff">Salvar</button>
@@ -94,67 +97,79 @@
                 </div><!--materiais-->
                 <div>
                     @foreach ($grupo['envios'] as $envio)
-                   <div class=" container-group mb-2"><!--postagem-->
-                        <div class="container-comentario">
+                    <div class=" container-group mb-2"><!--postagem-->
+                        <div class="container-postagem">
                             <div class="row">
+
                                 <div class="col-md-2" ><!--img do usuario da postagem-->
                                     <div class="img-perfil-post">
                                         <img src="/img/user-pic-exemple.jpg " height="100"/> 
                                     </div>
                                 </div><!--img do usuario da postagem-->
-                                <div class="col-md-10">
-                                     <div class="row"><!--conteudo da postagem-->
+                                <div class="col-8 col-sm-9 col-md-10 "><!--conteudo da postagem-->
+                                     <div class="row">
                                         <div class="col-md-12">
                                              <div class="row"><!--assunto da postagem-->
                                                 <div class="col-md-12">
                                                     <div class="postagem-group">
-                                                         {{$envio->assunto}}
-                                                   </div>
+                                                        <div class="row">
+                                                            <div class="col-md-10">
+                                                                <a href="{{action('EnviosController@detalhes',$envio->id)}}" title="Ver postagem completa"><h5> {{$envio->assunto}}</h5></a>
+                                                                <p id="keywords">
+                                                                    @foreach($envio->palavras as $palavra)
+                                                                        {{$palavra['palavra']}},
+                                                                    @endforeach
+                                                                </p>
+                                                            </div>        
+                                                            <div class="col-md">
+                                                                 <p id="data-hora">{{ date('d/m/Y H:i:s',  strtotime($envio->created_at))}}</p>
+                                                            </div>
+                                                        </div>                                                 
+                                                    </div>                                                    
                                                 </div>
                                             </div>
                                             <div class="row"><!--conteudo da postagem-->
                                                 <div class="col-md-12">
                                                     <div class="postagem-group">
-                                                         {{$envio->info}}
+                                                         <p id="conteudo"> {{$envio->info}}</p>
                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
-                                     </div><!--conteudo da postagem-->
+                                     </div>
                                      
                                      <div class="row"><!--footer da postagem-->
                                         <div class="col-md-12">
                                             <div class="postagem-footer-group">
-                                                <div class="row">
-                                                    <div class="col-md-12">
-                                                        <div class="">
-                                                            @foreach($envio->palavras as $palavra)
-                                                                {{$palavra['palavra']}},
-                                                            @endforeach
-                                                        </div>
-                                                    </div>
-                                                </div>
-
-                                                <div class="row">
-                                                    <div class="col-md-12">
-                                                        <div class="">
-                                                            <p>{{$envio->created_at}}</p>
-                                                        </div>
-                                                    </div>
-                                                 </div>
                                                  <div class="row">
                                                     <div class="col-md-12">
-                                                        <div class="">Comentários:
-                                                            <p>{{count($envio->comentarios)}}</p>
-                                                            <p><a href="{{action('EnviosController@detalhes',$envio->id)}}"> Ver mais...</a></p>
+                                                        <div class="row">
+                                                            <div class="col-md-8">
+                                                                <a href="{{action('EnviosController@detalhes',$envio->id)}}" title="Ver comentários">
+                                                                    @if(count($envio->comentarios) == 0)
+                                                                        Não há comentários
+                                                                    @endif
+                                                                    @if(count($envio->comentarios) > 1)
+                                                                        {{count($envio->comentarios)}} comentários
+                                                                    @endif
+                                                                    @if(count($envio->comentarios) == 1)
+                                                                        {{count($envio->comentarios)}} comentário
+                                                                    @endif
+                                                                 </a>
+                                                            </div>
+                                                            <div class="col-md" id="controls">
+                                                                <a href="" class="mr-1" id="btn-edit"><i class="fa fa-edit" id="btn-edit-ico"></i> Editar</a>
+                                                                <a href="" id="btn-close"><i class="fa fa-close" id="btn-close-ico" ></i> Excluir</a>
+                                                            </div>
                                                         </div>
+
                                                     </div>
                                                  </div>
                                             </div>
                                         </div>
                                     </div><!--footer da postagem-->
 
-                                </div>                     
+                                </div> <!--conteudo da postagem-->                    
                             </div>   
                         </div>                                                        
                    </div><!--postagem-->
@@ -162,6 +177,9 @@
 
 
                 </div>
+
+
+
                 <div class="col-md-0">
                     
                 </div>
@@ -173,38 +191,5 @@
     </div>
     </div>
     
-
-
-
-
-        <!--h1>{{$grupo['grupo']->nome}}</h1>
-        @if(isset($grupo['grupo']->conviteResposta))
-            @if($grupo['grupo']->conviteResposta)
-                <div class="alert alert-success">
-                    Convite enviado com sucesso
-                </div>
-            @else
-                <div class="alert alert-danger">
-                    Usuário já convidado
-                </div>
-            @endif
-        @endif
-        <ul>
-            <li>
-                @if($grupo['grupo']->classificacao == null)
-                    Não possui
-                @else
-                    {{$grupo['grupo']->classificacao}}
-                @endif
-            </li>
-        </ul>
-
-        <a href="{{action('GruposController@alterar',$grupo['grupo']->id)}}">
-            Alterar
-        </a>
-
-        <a href="{{action('GruposController@convidarUsuario',$grupo['grupo']->id)}}">
-            Convidar Membros
-        </a-->
    @endsection
     <b></b>
