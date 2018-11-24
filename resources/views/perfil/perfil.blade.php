919557bc68263bfab2a690337a7a327ec668d41b
yssacst <rayssacoosta@gmail.com>
2018-11-24 01:17:19 -0200

    perfil

--- resources/views/perfil/perfil.blade.php
+++ resources/views/perfil/perfil.blade.php
@@ -1,41 +1,28 @@
 @extends('layout.mainlayout')
 @section('content')
 <div class="container-fluid ">
-   <div class="row">
-      <div class="col-md-12">
-         <div class="row">
-            <div class="col-md-12 pl-0 pr-0">
-               <div class="profile-header text-center">
-                     <div class="container-inner">
-                        <img class="rounded-circle media-object" src="/img/pic-perfil.jpg">
-                     </div>
+   <div class="row title header-perfil">
+      <div class="col-md-12 body-perfil centralizado">
+         <div class="content-perfil " title="Editar Perfil">
+            <div class="row">
+               <div class="col-md-12 centralizado">
+                  <div class="img-perfil ce">
+                     <img src="/img/pic-perfil.jpg">
+                  </div>
                </div>
             </div>
-         </div>
-         <div class="row">
-            <div class="col-md-12">
-               <div class="container-bc mt-4">
-                  <div class="card-perfil">
-                     <div class="card-body">
-                        <h1>Robson Nascimento <i class="fa fa-edit" title="Editar perfil"></i></h1>
-                        <h5>25 anos | Rio de Janeiro </h5>
-                        
-                        <p>"Comece de onde você está. Use o que você tiver. Faça o que você puder" – Arthur Ashe.</p>
-                        <div class="social">
-                           <ul>
-                              <li><a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
-                              <li><a href="#"><i class="fa fa-twitter-square" aria-hidden="true"></i></a> </li>
-                              <li><a href="#"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a></li>
-                           </ul>
-                        </div>
-                     </div>
+            <div class="row">
+               <div class="col-md-12 centralizado">
+                  <div class="text-perfil mt-4">
+                     <h3>Renan da Silva</h3>
+                     <small>25 anos - Rio de Janeiro</small>
+                     <p> “Comece de onde você está. Use o que você tiver. Faça o que você puder.“ - Arthur Ashe</p>
                   </div>
                </div>
             </div>
          </div>
       </div>
    </div>
-</div>
 </div>
 @endsection
 <b></b>
