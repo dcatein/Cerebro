0141bc657185e0c18b4751c2a8b4810e61401713
yssacst <38430578+yssacst@users.noreply.github.com>
2018-11-24 12:18:23 -0200

    telaconviteconfiguracaogrupotelaplanoestudo (#28)

--- resources/views/convites/form.blade.php
+++ resources/views/convites/form.blade.php
@@ -1,21 +1,21 @@
 @extends('layout.mainlayout')
     @section('content')
 
-<div class="container-fluid pt-2 px-5">
+<div class="container-fluid">
     <div class="row justify-content-end ">
-        <div class="col-md-12 pl-5" style="display:inline-block">   
-            <div class="row">
+        <div class="col-md-12 " >   
+            <div class="row title">
                 
-                <div class="col-md-10">
-                    <h2 class="title">Convide para o grupo  {{$grupo->nome}}</h2>
+                <div class="col-md-10 pl-5">
+                    <h2 >Convide para o grupo  {{$grupo->nome}}</h2>
                 </div>
-                <div class="col-md">
+                <div class="col-md pl-5">
                    <a href="{{action('GruposController@find',$grupo->id)}}" title="Voltar" class="btn fnt-white"><i class="mt-2 ml-5 fa fa-reply icon-size-15"></i> Voltar</a>
                 </div>
             </div>
             <div class="row"> 
-                <div class="col-md-12">
-                    <div class="container-group">
+                <div class="col-md-12 pl-5">
+                    <div class="container-group mt-3">
                         <div>
                             @if (isset($convite))
                                 <div class="alert-danger">
